6d8b630a48576dd404b1a48e23ab38b62a5645d8
Mike <mlee8343@gmail.com>
2023-01-05 16:52:41 -0800

    hi

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ export function Logo(props) {
         className="fill-emerald-400"
         d="M16 8a5 5 0 0 0-5-5H5a5 5 0 0 0-5 5v13.927a1 1 0 0 0 1.623.782l3.684-2.93a4 4 0 0 1 2.49-.87H11a5 5 0 0 0 5-5V8Z"
       />
-      <text x="35" y="20" className="text-2xl text-white">Zelar</text>
+      <text x="35" y="20" stroke="#FFFFFF" stroke-width="2.5px" className="text-2xl">Zelar</text>
     </svg>
   )
 }
